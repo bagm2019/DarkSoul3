9b0607ddb8c7372c2782543b2fbf4d8da1bfc6b5
bagm2019 <bagm@o2.pl>
2020-04-18 13:56:59 +0200

    Initial commit

---
+++ src/main/java/com/DarkSoul3/demo/DarkSoul3Application.java
@@ -0,0 +1,13 @@
+package com.DarkSoul3.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkSoul3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarkSoul3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/DarkSoul3/demo/DarkSoul3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DarkSoul3.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkSoul3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
